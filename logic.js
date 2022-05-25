aeb55e959a231dcaafc02922f4a371a63132f373
ostfars <v_minin@rambler.ru>
2022-05-25 11:15:27 +0300

    board created

--- logic.js
+++ logic.js
@@ -1,2 +1,13 @@
 let players = ['x', 'o'];
 let activePlayer = 0;
+
+function startGame() {
+  let gameField = [
+    ['', '', ''],
+    ['', '', ''],
+    ['', '', '']
+  ];
+  activePlayer = 0;
+  renderBoard(gameField);
+}
+
