a5f8e2d20227fd0fa4863faa099c46afb4fec053
ostfars <v_minin@rambler.ru>
2022-05-25 23:43:27 +0300

    multiple board sizes added

--- logic.js
+++ logic.js
@@ -1,13 +1,22 @@
 let players = ['x', 'o'];
 let activePlayer = 0;
+let gameField = [];
+
+let boardSize = 3;
+
+function createGameField() {
+  let row = [];
+  for (let i=0; i<boardSize; i++) {
+    row.push('')
+  }
+  for (let i=0; i<boardSize; i++) {
+    gameField.push(row)
+  }
+  return gameField
+}
 
 function startGame() {
-  let gameField = [
-    ['', '', ''],
-    ['', '', ''],
-    ['', '', '']
-  ];
+  gameField = createGameField();
   activePlayer = 0;
   renderBoard(gameField);
 }
-
